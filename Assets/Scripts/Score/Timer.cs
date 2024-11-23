b66e56cfd75b900b13edd2b30241e8c24a8b8449
Alec Tugeon <123414117+Alec-T1@users.noreply.github.com>
2024-11-23 13:53:30 -0500

    Timer/Score Multiplier Implemented
    -Stopwatch written in Code using Delta Time
    -Multiplier that is static before and after a time frame
    -Multiplier that depreciates during a defined time frame

---
+++ Assets/Scripts/Score/Timer.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    private float currentTime = 0;
+    TimeSpan time = TimeSpan.Zero;
+    private float PlayerScoreMultiplier;
+
+    //ADJUST THIS TO CHANGE THE MAXIMUM SCORE MULTIPLIER
+    private float PlayerScoreMultiplierMAX = 6;
+
+    //ADJUST THESE TO ACCOUNT FOR PAR TIME (Remember: 1 Minute is 60 Seconds, so do the math to implement)
+    private int ScoreMultiDepreciateStart = 5;
+    private int ScoreMultiDepreciateEnd = 35;
+
+    void Update()
+    {
+        currentTime = currentTime + Time.deltaTime;
+        time = TimeSpan.FromSeconds(currentTime);
+
+        if ( (int) currentTime <= ScoreMultiDepreciateStart)
+        {
+            PlayerScoreMultiplier = PlayerScoreMultiplierMAX;
+        }
+        else if ((int) currentTime > ScoreMultiDepreciateStart && (int) currentTime < ScoreMultiDepreciateEnd)
+        {
+        //It Calculates the Amount of Time Remaining before the 1x Multiplier by subtracting the Time that's passed since the Start of the Depreciation from the Total Time Gap of the Depreciation.
+        //It then takes that number and divides it by the total time so that we convert it into a percentage.
+         PlayerScoreMultiplier = ((float)(ScoreMultiDepreciateEnd - ScoreMultiDepreciateStart) - (currentTime - (float) ScoreMultiDepreciateStart)) / (float) (ScoreMultiDepreciateEnd-ScoreMultiDepreciateStart);
+         //The Percentage is then applied to the Maximum Multiplier (the -1 is to account for the fact we end at 1 and not 0)
+         //The +1 is likewise also to account for that fact
+         PlayerScoreMultiplier = PlayerScoreMultiplier * (PlayerScoreMultiplierMAX - 1) +1;
+        }
+        else if ((int) currentTime >= ScoreMultiDepreciateEnd)
+        {
+            PlayerScoreMultiplier = 1;
+        }
+
+
+        //Debug.Log(currentTime);
+        Debug.Log(PlayerScoreMultiplier);
+
+
+    }
+}
