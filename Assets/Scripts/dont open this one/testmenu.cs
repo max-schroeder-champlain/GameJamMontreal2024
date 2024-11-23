963c7975701dfb8f7d8599ca5bd6be5d41587215
grce-grdll <ggardella101@gmail.com>
2024-11-23 14:57:59 -0500

    TestMenu
    it works!

--- Assets/Scripts/dont open this one/testmenu.cs
+++ Assets/Scripts/dont open this one/testmenu.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class testmenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ButtonOne()
     {
-        
+        SceneManager.LoadScene("DialogueScene"); 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ButtonTwo()
     {
-        
+        SceneManager.LoadScene("BoxThrow"); 
     }
 }
